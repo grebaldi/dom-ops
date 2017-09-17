8bf76b6783e4a3ef9401f07bcb452d6261c5942b
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2017-09-17 11:21:15 +0200

    feat: Create index file

---
+++ index.js
@@ -0,0 +1,11 @@
+import find from './src/find';
+import findOne from './src/findOne';
+import listen from './src/listen';
+import listenOnce from './src/listenOnce';
+
+export {
+	find,
+	findOne,
+	listen,
+	listenOnce
+};
